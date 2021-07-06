7b0aca67641392bbda400100a59315936acf7efe
JohnnyJayJay <johnnyjayjay02@gmail.com>
2021-07-06 21:08:57 +0200

    Remove snapshot from version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject instant-poll "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject instant-poll "0.1.0"
+  :description "Discord app to create live polls"
+  :url "https://github.com/JohnnyJayJay/instant-poll"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [http-kit "2.5.3"]
