b3aa83db06b13595af3bc25b083e43099c75d73e
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-13 12:48:52 +0100

    Use slash in component ns

--- src/instant_poll/component.clj
+++ src/instant_poll/component.clj
@@ -1,12 +1,13 @@
 (ns instant-poll.component
   (:require [clojure.string :as string]
             [instant-poll.poll :as polls]
-            [instant-poll.interactions :refer [ephemeral-response normal-response update-message-response]]
             [instant-poll.state :refer [discord-conn config polls]]
             [discljord.messaging :as discord]
             [discljord.util :refer [parse-if-str]]
             [discljord.formatting :as discord-fmt]
-            [discljord.permissions :as discord-perms]))
+            [discljord.permissions :as discord-perms]
+            [slash.component.structure :as cmp]
+            [slash.response :as rsp]))
 
 (def option-id-separator " ")
 (def option-id-separator-pattern (re-pattern option-id-separator))
@@ -20,19 +21,12 @@
 (defn make-components [{:keys [options id] :as poll}]
   (concat
    (for [option-group (partition-all 5 options)]
-     {:type 1
-      :components
+     (apply
+      cmp/action-row
       (for [[key _] option-group]
-        {:type 2
-         :style 1
-         :label key
-         :custom_id (str id option-id-separator key)})})
-   [{:type 1
-     :components
-     [{:type 2
-       :style 4
-       :label "Close Poll"
-       :custom_id (str close-prefix id)}]}]))
+        (cmp/button :primary (str id option-id-separator key) :label key))))
+   [(cmp/action-row
+     (cmp/button :danger (str close-prefix id) :label "Close Poll"))]))
 
 (defn handle-button-press
   [{{{user-id :id} :user :keys [permissions]} :member
@@ -44,12 +38,12 @@
       (if (= option :close)
         (if (or (= user-id creator-id) (discord-perms/has-permission-flag? :manage-messages (parse-if-str permissions)))
           (let [poll (polls/close-poll! poll-id)]
-            (update-message-response
+            (rsp/update-message
              {:content (str (polls/render-poll poll (:bar-length config)) \newline
                             "Poll closed <t:" (quot (System/currentTimeMillis) 1000) ":R> by " (discord-fmt/mention-user user-id) \.)
               :components []}))
-          (ephemeral-response {:content "You do not have permission to close this poll."}))
+          (-> {:content "You do not have permission to close this poll."} rsp/channel-message rsp/ephemeral))
         (let [updated-poll (polls/toggle-vote! poll-id user-id option)]
           (swap! polls update poll-id assoc :channel-id channel-id :message-id message-id)
-          (update-message-response {:content (str (polls/render-poll updated-poll (:bar-length config)) \newline (polls/close-notice updated-poll true))})))
-      (ephemeral-response {:content "This poll isn't active anymore."}))))
+          (rsp/update-message {:content (str (polls/render-poll updated-poll (:bar-length config)) \newline (polls/close-notice updated-poll true))})))
+      (-> {:content "This poll isn't active anymore."} rsp/channel-message rsp/ephemeral))))
