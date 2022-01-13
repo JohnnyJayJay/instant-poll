50cd0c76ec833e018c2d542098adb091d7dfdb18
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-13 21:17:59 +0100

    Add show votes button to components

--- src/instant_poll/component.clj
+++ src/instant_poll/component.clj
@@ -14,39 +14,70 @@
 (defn parse-custom-id [custom-id]
   (string/split custom-id action-separator-pattern))
 
-(defn make-components [{:keys [options] :as _poll}]
+(defn make-components [{:keys [options open] :as _poll}]
   (concat
    (for [option-group (partition-all 5 options)]
      (apply
       cmp/action-row
       (for [[key _] option-group]
         (cmp/button :primary (str "vote" action-separator key) :label key))))
-   [(cmp/action-row
-     (cmp/button :danger "close" :label "Close Poll"))]))
+   [(apply
+     cmp/action-row
+     (cond-> []
+       open (conj (cmp/button :secondary "show-votes" :label "Show Votes" :emoji {:name "🔎"}))
+       true (conj (cmp/button :danger "close" :label "Close Poll" :emoji {:name "🔒"}))))]))
+
+(defmulti poll-action (fn [action _interaction _poll _options] action))
+
+(defmethod poll-action "vote"
+  [_ {{{user-id :id} :user} :member {message-id :id :keys [channel-id]} :message :as _interaction} {:keys [id] :as _poll} [option]]
+  (let [updated-poll (polls/toggle-vote! id user-id option)]
+    (swap! polls update id assoc :channel-id channel-id :message-id message-id)
+    (rsp/update-message {:content (str (polls/render-poll updated-poll (:bar-length config)) \newline (polls/close-notice updated-poll true))})))
+
+(defn abbreviate [s n]
+  (if (< (count s) n)
+    s
+    ()))
+
+(defmethod poll-action "show-votes"
+  [_ {} {:keys [votes] :as _poll} _]
+  (-> {:content
+       (let [msg (str
+                  "**Here are the individual votes for this poll:**\n"
+                  (string/join
+                   "; "
+                   (map (fn [[user-id options]]
+                          (str (discord-fmt/mention-user user-id) ": " (discord-fmt/code (string/join ", " options))))
+                        votes)))]
+         (if (> (count msg) 2000)
+           "Sorry, I can't display the votes, there are too many."
+           msg))
+       :allowed_mentions {}}
+      rsp/channel-message
+      rsp/ephemeral))
+
+(defmethod poll-action "close"
+  [_ {{{user-id :id} :user :keys [permissions]} :member :as _interaction} {:keys [id creator-id] :as _poll} _]
+  (if (or (= user-id creator-id) (discord-perms/has-permission-flag? :manage-messages (parse-if-str permissions)))
+    (let [poll (polls/close-poll! id)]
+      (rsp/update-message
+       {:content (str (polls/render-poll poll (:bar-length config)) \newline
+                      "Poll closed " (discord-fmt/timestamp (quot (System/currentTimeMillis) 1000) :relative-time)
+                      " by " (discord-fmt/mention-user user-id) \.)
+        :components []}))
+    (-> {:content "You do not have permission to close this poll."} rsp/channel-message rsp/ephemeral)))
+
+(defmethod poll-action :default
+  [action _ _ _]
+  (-> {:content (str "Unknown action '" action "'. This is a bug. Please report this to the developers.")}))
 
 (defn handle-button-press
-  [{{{user-id :id} :user :keys [permissions]} :member
-    {:keys [custom-id]} :data
-    {message-id :id {poll-id :id} :interaction :keys [channel-id]} :message
-    :as _interaction}]
-  (if-let [{:keys [creator-id] :as _poll} (polls/find-poll poll-id)]
-    (let [[action option] (parse-custom-id custom-id)]
-      (case action
-        "close"
-        (if (or (= user-id creator-id) (discord-perms/has-permission-flag? :manage-messages (parse-if-str permissions)))
-          (let [poll (polls/close-poll! poll-id)]
-            (rsp/update-message
-             {:content (str (polls/render-poll poll (:bar-length config)) \newline
-                            "Poll closed " (discord-fmt/timestamp (quot (System/currentTimeMillis) 1000) :relative-time)
-                            " by " (discord-fmt/mention-user user-id) \.)
-              :components []}))
-          (-> {:content "You do not have permission to close this poll."} rsp/channel-message rsp/ephemeral))
-
-        "vote"
-        (let [updated-poll (polls/toggle-vote! poll-id user-id option)]
-          (swap! polls update poll-id assoc :channel-id channel-id :message-id message-id)
-          (rsp/update-message {:content (str (polls/render-poll updated-poll (:bar-length config)) \newline (polls/close-notice updated-poll true))}))
-
-        (-> {:content (str "Unknown action '" action "'. This is a bug. Please report this to the developers.")})))
+  [{{:keys [custom-id]} :data
+    {{poll-id :id} :interaction} :message
+    :as interaction}]
+  (if-let [poll (polls/find-poll poll-id)]
+    (let [[action & options] (parse-custom-id custom-id)]
+      (poll-action action interaction poll options))
     ;; if poll not found
     (-> {:content "This poll isn't active anymore."} rsp/channel-message rsp/ephemeral)))
