73d0ee143d52730d021641018d79f78806b242f4
JohnnyJayJay <johnnyjayjay02@gmail.com>
2023-06-03 14:55:36 +0200

    Update Clojure to 1.11.1

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Discord app to create live polls"
   :url "https://github.com/JohnnyJayJay/instant-poll"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [http-kit "2.5.3"]
                  [ring/ring-core "1.9.3"]
                  [ring/ring-json "0.5.1"]
