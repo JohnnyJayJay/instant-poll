0af4fcbf0b68f287ee6da85688103c73f853ca84
JohnnyJayJay <johnnyjayjay02@gmail.com>
2023-07-11 22:04:40 +0200

    Add support for updating commands via command line argument

--- src/instant_poll/handler.clj
+++ src/instant_poll/handler.clj
@@ -1,16 +1,19 @@
 (ns instant-poll.handler
   (:gen-class)
-  (:require [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
-            [ring.util.response :refer [response]]
-            [ring-discord-auth.ring :refer [wrap-authenticate]]
-            [instant-poll.state :refer [config]]
-            [instant-poll.command :refer [handle-command]]
-            [instant-poll.component :refer [handle-button-press handle-add-option-form-submit]]
-            [mount.core :as mount]
-            [org.httpkit.server :as http-server]
-            [discljord.util :as discord-util]
-            [slash.core :as slash]
-            [slash.webhook :refer [webhook-defaults]]))
+  (:require
+   [clojure.tools.logging :as log]
+   [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+   [ring.util.response :refer [response]]
+   [ring-discord-auth.ring :refer [wrap-authenticate]]
+   [instant-poll.state :as state :refer [config]]
+   [instant-poll.command :refer [handle-command poll-command]]
+   [instant-poll.component :refer [handle-button-press handle-add-option-form-submit]]
+   [mount.core :as mount]
+   [org.httpkit.server :as http-server]
+   [discljord.util :as discord-util]
+   [discljord.messaging :as dm]
+   [slash.core :as slash]
+   [slash.webhook :refer [webhook-defaults]]))
 
 (def slash-handlers
   (assoc webhook-defaults
@@ -24,7 +27,19 @@
 (defn wrap-clean-json [handler]
   #(handler (update % :body discord-util/clean-json-input)))
 
-(defn -main [& _args]
+(defn -main [arg]
+  (when (= arg "update")
+    (log/info "Updating global slash commands")
+    (mount/start #'state/config #'state/discord-conn)
+    (let [result @(dm/bulk-overwrite-global-application-commands! state/discord-conn [poll-command])]
+      (mount/stop)
+      (when (instance? Exception result)
+        (log/error result "Updating commands was unsuccessful.")
+        (System/exit 1)))
+    (log/info "Commands updated successfully. It may take up to an hour and reloading your Discord client to see the changes.")
+    (System/exit 0))
+
+  (log/info "Starting instant-poll")
   (mount/start)
   (http-server/run-server
    (-> #'handler
